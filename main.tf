fa8a23e234595c275199e108eca79142ce424ebb
Joao Paulo <jppreti@gmail.com>
2022-04-21 13:18:24 -0400

    feat: Adicionado um NAT Gateway e tabela de roteamento para a subrede privada

--- main.tf
+++ main.tf
@@ -37,3 +37,29 @@ resource "aws_subnet" "PrivateSubnet" {
 	availability_zone = var.private_zone
 	cidr_block = var.private_subnet
 }
+
+#Criando um Elastic IP para gerenciamento de IPs
+resource "aws_eip" "EIP" {
+	vpc = true
+}
+
+#Criando um NAT Gateway para permitir o acesso a recursos externos
+resource "aws_nat_gateway" "NATgw" {
+	allocation_id = aws_eip.EIP.id
+	subnet_id = aws_subnet.PublicSubnet.id
+}
+
+#Tabela de roteamento para a subnet privada
+resource "aws_route_table" "PrivateRT" {
+	vpc_id = aws_vpc.Main.id
+	route {
+		cidr_block = "0.0.0.0/0"
+		nat_gateway_id = aws_nat_gateway.NATgw.id
+	}
+}
+
+#Associando a tabela de roteamento com a subrede privada
+resource "aws_route_table_association" "PrivateRTassociation" {
+	subnet_id = aws_subnet.PrivateSubnet.id
+	route_table_id = aws_route_table.PrivateRT.id
+}
