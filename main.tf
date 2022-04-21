4ac174c4fb0fdb016e2ccb71f205f9f7dba12aa5
Joao Paulo <jppreti@gmail.com>
2022-04-21 12:38:47 -0400

    docs: Documentados os recursos de VPC e Internet Gateway

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
+#Criando uma VPC padrão para 250 hosts
 resource "aws_vpc" "Main" {
 	cidr_block = var.vpc_cidr
 	instance_tenancy = "default"
 }
 
+#Criando e vinculando um gateway de internet para a VPC
 resource "aws_internet_gateway" "IGW" {
 	vpc_id = aws_vpc.Main.id
 }
+
